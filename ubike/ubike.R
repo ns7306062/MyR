06ef428defbe0aeb7dc2e8e18512d4e69d56a4e2
m0904107 <ns7306062@gmail.com>
2020-10-29 20:47:16 +0800

    20201029 updated

---
+++ ubike/ubike.R
@@ -0,0 +1,6 @@
+setwd("D:/20201029/ubike")
+install.packages('jsonlite')
+library(jsonlite)
+download.file("https://quality.data.gov.tw/dq_download_json.php?nid=123026&md5_url=4d8de527a0bcd8a7b1aeae91120f021d","ubikeNTP.json")
+jdata <- fromJSON("ubikeNTP.json")
+table(jdata$sarea)
